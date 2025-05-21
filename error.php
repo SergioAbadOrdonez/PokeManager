814adca7ecafd29bf6973761e11cc8c7a0f23dae
Sergio Abad <sergioabaddaw@gmail.com>
2025-05-21 16:30:45 +0200

    Mejoras visuales en la página de error: centrado, imagen y mensaje

--- error.php
+++ error.php
@@ -1,10 +1,22 @@
 <?php
 require_once 'includes/header.inc.php';
+// Frases aleatorias de Pokémon
+$frases = [
+    "¡Pikachu está triste por tu error!",
+    "¡Parece que has lanzado una Pokéball y ha fallado!",
+    "¡Oh no! Un Snorlax bloquea el registro.",
+    "¡Charmander se ha quedado sin fuego! Intenta de nuevo.",
+    "¡Un Zubat te ha confundido! Prueba otra vez.",
+    "¡El Equipo Rocket se ha llevado tu formulario!",
+    "¡Un Ditto ha cambiado tus datos!",
+    "¡Psyduck tiene dolor de cabeza por este error!"
+];
+$frase = $frases[array_rand($frases)];
 ?>
 
 <main>
     <section class="error">
-        <h2>Ooops... ¡algo ha fallado!</h2>
+        <h2 class="frase-pokemon"><?php echo $frase; ?></h2>
         <div class="alerta-error">
             <?php echo isset($_SESSION['error']) ? $_SESSION['error'] : 'Ha ocurrido un error'; ?>
         </div>
